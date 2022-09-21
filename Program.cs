ec8c2285fb7a6eff66d0ccef94f58c6ebead1f77
Anastasya <gavrilovaanastasya@yandex.ru>
2022-09-21 11:47:42 +0300

    Program tree: вывод чисел от -N до N

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа, принимающая на вход одно число N, а на выходе показывает все целые числа в промежутке от -N до N
+
+Console.WriteLine("Введи число: ");
+int N = int.Parse(Console.ReadLine());
+int N2 = -N;
+
+while(N2<=N)
+{
+ Console.WriteLine(N2+" ");
+ N2++;
+}
